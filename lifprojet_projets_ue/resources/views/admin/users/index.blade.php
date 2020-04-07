9dfed92a9b2e67f29117f8111c574a97aeb09c89
LY VICHITH p1706375 <vichith.ly@etu.univ-lyon1.fr>
2020-04-07 17:56:05 +0200

    Gate Users access & Hiding  UI element

--- lifprojet_projets_ue/resources/views/admin/users/index.blade.php
+++ lifprojet_projets_ue/resources/views/admin/users/index.blade.php
@@ -32,21 +32,24 @@
                                 <td>{{ implode(', ', $user->roles()->pluck('name')->toArray()) }}</td>
                                 
                                 <td>
+                                    
                                     @can('edit-users')
                                         <a href="{{ route('admin.users.edit', $user->id) }}"> 
                                             <button type="button" class="btn btn-primary float-left">Edit</button>
                                         </a>
                                     @endcan
                                     
+                                    @can('delete-users')    
+                                        <form action="{{ route('admin.users.destroy', $user) }}" method="POST" class="float-left">
+                                            @csrf
 
-                                    <form action="{{ route('admin.users.destroy', $user) }}" method="POST" class="float-left">
-                                        @csrf
+                                            {{ method_field('DELETE') }}
 
-                                        {{ method_field('DELETE') }}
+                                            <button type="submit" class="btn btn-warning">Delete</button>
+                                        
+                                        </form>
+                                    @endcan
 
-                                        <button type="submit" class="btn btn-warning">Delete</button>
-
-                                    </form>
                                 </td>
                             </tr>
 
