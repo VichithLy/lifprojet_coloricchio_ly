6c6e736e1bd203220953e0a9e10f42bc3168eca0
LY VICHITH p1706375 <vichith.ly@etu.univ-lyon1.fr>
2020-04-09 15:40:19 +0200

    debut creation ue

--- lifprojet_projets_ue/resources/views/home.blade.php
+++ lifprojet_projets_ue/resources/views/home.blade.php
@@ -2,36 +2,12 @@
 
 @section('content')
     <div class="row justify-content-center">
-        <div class="col-md-9">
+        <div class="col-md-12">
             <div class="card">
                 
                 <div class="card-header">Projects panel</div>
 
-                <!-- =================== UPLOAD DE PROJETS ====================== -->
-
-                <div class="font-weight-bold text-center h4 mt-3">
-                    Ajouter des projets
-                </div>
-
-                <nav class="navbar navbar-light border border-dark rounded">
-                    <div class="input-group input-group-sm">
-
-                        <div class="input-group-prepend mb-2">
-                            <span class="input-group-text" id="inputGroup-sizing-sm">UE</span>
-                        </div>
-                        <div class="col-xs-3 mr-sm-5">
-                            <select class="form-control form-control-sm">
-                                <option>LIFO63</option>
-                                <option>LIFAP6</option>
-                            </select>
-                        </div>
-                    </div>
-
-                    <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="customFile">
-                        <label class="custom-file-label" for="customFile">Upload a project</label>
-                    </div>
-                </nav>
+                
 
                 <div class="card-body">
                     @if (session('status'))
@@ -39,35 +15,91 @@
                             {{ session('status') }}
                         </div>
                     @endif  
-                    
-                    <!-- =================== BARRE DE RECHERCHE ====================== -->
 
-                    <nav class="navbar navbar-light mb-5">
-                        <form class="form-inline mb-2">
-                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                        </form>
+
+                    <!-- =================== UPLOAD DE PROJETS ====================== -->
+
+                    <nav class="navbar navbar-light border rounded mb-4">
+                        <div class="font-weight-bold h5 mb-3">
+                            Ajouter des projets
+                         </div>
 
                         <div class="input-group input-group-sm">
 
-                            <div class="input-group-prepend ">
-                                <span class="input-group-text" id="inputGroup-sizing-sm">UE</span>
+                            <div class="input-group-prepend mb-2">
+                                <span class="input-group-text rounded" id="inputGroup-sizing-sm">UE</span>
                             </div>
-                            <div class="col-xs-3 mr-sm-5">
+                            <div class="mr-sm-5">
                                 <select class="form-control form-control-sm">
                                     <option>LIFO63</option>
                                     <option>LIFAP6</option>
                                 </select>
                             </div>
 
-                            <div class="input-group-prepend">
-                                <span class="input-group-text" id="inputGroup-sizing-sm">Tier par</span>
+                            <div class="input-group-prepend mb-2">
+                                <span class="input-group-text rounded" id="inputGroup-sizing-sm">Année</span>
                             </div>
-                            <div class="col-xs-3">
+                            <div class="mr-sm-5">
                                 <select class="form-control form-control-sm">
-                                <option>Ordre alphabétique</option>
-                                <option>Note</option>
-                            </select>
+                                    <option>Aucune</option>
+                                    <option>2018</option>
+                                    <option>2019</option>
+                                    <option>2020</option>
+                                </select>
+                            </div>
+
+                            
+                        </div>
+
+                        <div class="custom-file">
+                            <input type="file" class="custom-file-input" id="customFile">
+                            <label class="custom-file-label" for="customFile">Upload a project</label>
+                        </div>
+                    </nav>
+
+                    
+                    <!-- =================== BARRE DE RECHERCHE ====================== -->
+
+                    <nav class="navbar navbar-light mb-3">
+                        <form class="form-inline mb-2">
+                            <input class="form-control mr-sm-2" type="search" placeholder="Search a projet" aria-label="Search">
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+
+                        <div class="input-group input-group-sm">
+
+                            <div class="input-group-prepend ">
+                                <span class="input-group-text rounded" id="inputGroup-sizing-sm">UE</span>
+                            </div>
+                            <div class="mr-sm-5">
+                                <select class="form-control form-control-sm">
+                                    <option>Tous</option>
+                                    <option>LIFO63</option>
+                                    <option>LIFAP6</option>
+                                </select>
+                            </div>
+
+                            <div class="input-group-prepend ">
+                                <span class="input-group-text rounded" id="inputGroup-sizing-sm">Année</span>
+                            </div>
+                            <div class="mr-sm-5">
+                                <select class="form-control form-control-sm">
+                                    <option>Toutes</option>
+                                    <option>2018</option>
+                                    <option>2019</option>
+                                    <option>2020</option>
+                                </select>
+                            </div>
+
+                            <div class="input-group-prepend">
+                                <span class="input-group-text rounded" id="inputGroup-sizing-sm">Trier par</span>
+                            </div>
+                            <div>
+                                <select class="form-control form-control-sm">
+                                    <option>Date d'ajout</option>
+                                    <option>Ordre alphabétique</option>
+                                    <option>Note</option>
+                                </select>
                             </div>
 
                         </div>
@@ -102,12 +134,21 @@
                             </div>  
                             
                             <div class="card-body">
-                                <h5 class="card-title">Titre</h5>
-                                <p class="card-text">Cette image représente Dora ainsi que son meilleur ami Babouche. Ces deux tourtereaux ne peuvent se séparer.</p>
+                                <h5 class="card-title text-center"><strong>Dora & Babouche</strong></h5>
+
+                                <ul class="list-group ">
+                                    <li class="list-group-item">UE : <strong>Dessins animés</strong></li>
+                                    <li class="list-group-item">Année : <strong>2020</strong></li>
+                                </ul>
+
+                                <div class="card-text mt-3">
+                                    <p><u>Description :</u></p> 
+                                    Cette image représente Dora ainsi que son meilleur ami Babouche. Ces deux tourtereaux ne peuvent se séparer.
+                                </div>
                             </div>
                             <ul class="list-group list-group-flush">
                                 <li class="list-group-item">README</li>
-                                <li class="list-group-item">Note/20</li>
+                                <li class="list-group-item">Note : <strong>20/20</strong></li>
                             </ul>
                             <div class="card-body">
                                 <div><a href="#" class="card-link">Télécharger le projet</a></div>
@@ -125,6 +166,7 @@
                         <!------------------------------------------------------------------------->
 
                         <div class="card border-dark">
+                            
                             <!-- Images -->
                             <div id="carouselExampleControls1" class="carousel slide" data-ride="carousel">
                                 <div class="carousel-inner">
@@ -149,12 +191,22 @@
                             </div>  
                             
                             <div class="card-body">
-                                <h5 class="card-title">Titre</h5>
-                                <p class="card-text">Cette image représente Dora ainsi que son meilleur ami Babouche. Ces deux tourtereaux ne peuvent se séparer.</p>
+                                <h5 class="card-title text-center"><strong>Dora & Babouche</strong></h5>
+                                
+                                <ul class="list-group ">
+                                    <li class="list-group-item">UE : <strong>Dessins animés</strong></li>
+                                    <li class="list-group-item">Année : <strong>2020</strong></li>
+                                </ul>
+
+                                <div class="card-text mt-3">
+                                    <p><u>Description :</u></p> 
+                                    Cette image représente Dora ainsi que son meilleur ami Babouche. Ces deux tourtereaux ne peuvent se séparer.
+                                </div>
                             </div>
+
                             <ul class="list-group list-group-flush">
                                 <li class="list-group-item">README</li>
-                                <li class="list-group-item">Note/20</li>
+                                <li class="list-group-item">Note : <strong>20/20</strong></li>
                             </ul>
                             <div class="card-body">
                                 <div><a href="#" class="card-link">Télécharger le projet</a></div>
@@ -195,12 +247,21 @@
                             </div>  
                             
                             <div class="card-body">
-                                <h5 class="card-title">Titre</h5>
-                                <p class="card-text">Cette image représente Dora ainsi que son meilleur ami Babouche. Ces deux tourtereaux ne peuvent se séparer.</p>
+                                <h5 class="card-title text-center"><strong>Dora & Babouche</strong></h5>
+                                
+                                <ul class="list-group ">
+                                    <li class="list-group-item">UE : <strong>Dessins animés</strong></li>
+                                    <li class="list-group-item">Année : <strong>2020</strong></li>
+                                </ul>
+
+                                <div class="card-text mt-3">
+                                    <p><u>Description :</u></p> 
+                                    Cette image représente Dora ainsi que son meilleur ami Babouche. Ces deux tourtereaux ne peuvent se séparer.
+                                </div>
                             </div>
                             <ul class="list-group list-group-flush">
                                 <li class="list-group-item">README</li>
-                                <li class="list-group-item">Note/20</li>
+                                <li class="list-group-item">Note : <strong>20/20</strong></li>
                             </ul>
                             <div class="card-body">
                                 <div><a href="#" class="card-link">Télécharger le projet</a></div>
